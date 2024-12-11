5c29fb0d4a4a742a43b45813ec9391758c3072f5
Arsheen14-sh <arsh.shaikh1612@gmail.com>
2024-12-11 16:33:57 +0530

    first

---
+++ list.py
@@ -0,0 +1,78 @@
+#list=[1,2,"a",4.5]
+#print(list)
+#list.append("str")
+#print(list)
+#list.pop(2)
+#print(list)
+#list1=[4,5]
+#list.extend(list1)
+#print(list)
+#print(list[-1])
+#print(list[2:])
+#print(list[-1:])
+#list.insert(3,"sarah")
+#print(list)
+#list.remove(4)
+#print(list)
+#list.reverse()
+#print(list)
+#list.remove(4.5)
+#print(list)
+
+#sorting lists
+
+book_list = ["Data Structure", "Algorithm", "Web dev"]
+'''print(sorted(book_list))
+print(book_list.index("Algorithm"))
+print(book_list.index("Web dev"))
+book_list = ['Data Structure',"Data Structure", 'Algorithm', 'Web dev']
+print(book_list.count('Data Structure'))
+
+print(book_list.remove('Data Structure'))
+
+prices = [10, 20, 30, 40, 50]
+doubled_price = []
+for i in prices:
+    doubled_price.append(i*2)
+print(doubled_price)
+
+doubled_price = [price * 2 for price in prices]
+print(doubled_price)
+
+str1 = "doc1.ppt"
+print(str1.split(".")[-1])
+file_name = ["doc1.ppt", "doc2.pdf", "doc3.jpg", "doc4.py"]
+[file.split(".")[-1] for file in file_name]
+print(file_name)
+
+email_address = ["aj@gmail.com", "sj@yahoo.com", "rj@yahoo.com", "mj@gmail.com"]
+
+print([email for email in email_address if email.endswith("@yahoo.com")])
+
+
+stack_of_plates = []
+stack_of_plates.append("plate1")
+stack_of_plates.append("plate2")
+stack_of_plates.append("plate3")
+stack_of_plates.append("plate4")
+print(stack_of_plates)
+print(stack_of_plates.pop())
+print(stack_of_plates)
+browsing_history = []
+browsing_history.append("home_page")
+browsing_history.append("about us")
+browsing_history.append("contact")
+print(browsing_history)
+print(browsing_history.pop())
+print(browsing_history)'''
+
+
+
+
+
+
+
+
+
+
+
