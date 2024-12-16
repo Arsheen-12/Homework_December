f5aa07f23aa568478ef0f3046cbdce65a2b94638
Arsheen-12 <arsh.shaikh1612@gmail.com>
2024-12-16 16:06:51 +0530

    Update Program_DT.py

--- Program_DT.py
+++ Program_DT.py
@@ -1,12 +1,12 @@
 #Python Data Types
 #1. Check the Type: Write a program to take an input from the user and print its data type.
-n=eval(input("Enter n:"))
+n=input("Enter n:")
 print("Data type is:",type(n))
 
 #2. Simple Arithmetic: Accept two numbers from the user and perform addition, subtraction, multiplication, 
 # and division.
-a=eval(input("Enter a:"))
-b=eval(input("Enter b:"))
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
 print("Addition is:",a+b)
 print("Subtraction is:",a-b)
 print("Multiplication is:",a*b)
